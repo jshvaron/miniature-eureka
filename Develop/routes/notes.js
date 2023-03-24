ed2cf8604c71b39b609bac9fe220d3fad58f7da4
jshvaron <jshva@icloud.com>
2023-03-24 15:57:24 -0500

    GET AND POST routes complete for notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const notesData = require('../db/db.json')
+
+
 
 // set route for notes.html
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 
 
-module.exports = router;
+
+module.exports = router
--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT  = 3001;
 const app = express();
 const path = require('path')
+const notesData = require('./db/db.json')
 
 
 //allows us to use static files in the public folder 
@@ -22,7 +23,18 @@ const notesRouter = require('./routes/notes');
     app.use('/notes', notesRouter);
 
 
-    
+//read an return from /api in JSON, logs 'I CAN READ'
+app.get('/api/notes', (req, res) => {
+    res.json(notesData)
+    console.info(' I CAN READ')
+});
+// POST to ^api via JSON
+app.post('/api/notes',(req, res) => {
+    res.json(`${req.method} request received`)
+    console.info('I CAN WRITE')
+});
+
+
 //logs if the server is working
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
